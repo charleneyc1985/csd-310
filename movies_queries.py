a56d09ff8067554e126f75f8474b3029ddd6039c
charleneyc1985 <111462887+charleneyc1985@users.noreply.github.com>
2022-11-26 22:46:29 -0800

    Update movies_queries.py

--- movies_queries.py
+++ movies_queries.py
@@ -1,3 +1,9 @@
+# Charlene Centeno - 11/26/2022 - Module 7
+# Movies: Table Queries
+# The first and second query is to select all the fields for the studio and genre tables.
+# The third query is to select the movie names for those movies that have a run time of less than two hours.
+# The fourth query is to get a list of film names, and directors grouped by director.
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -9,6 +15,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# select all the fields for the studio tables
+
 mycursor.execute("SELECT studio_id, studio_name FROM studio")
 
 studio = mycursor.fetchall()
@@ -19,6 +27,8 @@ for studio in studio:
     print("Studio ID: {} \nStudio Name: {}".format(studio[0], studio[1]))
     print()
 
+# select all the fields for the genre tables
+    
 mycursor.execute("SELECT genre_id, genre_name FROM genre")
 
 genre = mycursor.fetchall()
@@ -29,6 +39,8 @@ for genre in genre:
     print("Genre ID: {} \nGenre Name: {}".format(genre[0], genre[1]))
     print()
 
+# select the movie names for those movies that have a run time of less than two hours
+    
 mycursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < 120")
 
 film = mycursor.fetchall()
@@ -39,6 +51,8 @@ for film in film:
     print("Film Name: {} \nRuntime: {}".format(film[0], film[1]))
     print()
 
+# select a list of film names, and directors grouped by director
+    
 mycursor.execute("SELECT film_name, film_director FROM film ORDER BY film_director")
 
 film = mycursor.fetchall()
